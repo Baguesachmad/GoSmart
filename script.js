d66ecad1202bb348760be566ab13a5c81c4f6997
GoSmarttools <red.mediaonlinenusantara@gmail.com>
2026-02-16 23:31:41 +0700

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-// Kalau nanti perlu scroll animasi atau CTA,
 console.log("GoSmart Pages Ready 🎉");
+
+// Smooth scroll for "Fitur" button
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
